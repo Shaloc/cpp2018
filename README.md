7e292f938f77c92999ed6b7d25b108eefcad31d9
Shaloc <shalocn@outlook.com>
2018-11-08 18:33:32 +0800

    add lab3

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cpp2018
-(moved from gitee) cpp class 2018, assignments, labs and notes
+CPP class 2018, assignments, labs and notes
---
+++ labs/lab3/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by shaloc on 2018/11/8.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+}
